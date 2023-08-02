063b05d70b9d4b8841160857ea20ee86df0bffe1
victorlun <victor.lundqvist@se.experis.com>
2023-08-02 15:38:22 +0200

    fix: Lvl check in equip methods

--- src/main/java/heroes/Hero.java
+++ src/main/java/heroes/Hero.java
@@ -4,9 +4,9 @@ import java.util.*;
 
 public abstract class Hero {
     private final String name;
-    public int level = 1;
+    private int level = 1;
     protected HeroAttribute levelAttributes;
-    private Map<Slot, Item> equipment = new HashMap<Slot, Item>();
+    public Map<Slot, Item> equipment = new HashMap<Slot, Item>();
     private ArrayList<String> validWeaponTypes = new ArrayList<String>();
     private ArrayList<String> validArmorTypes = new ArrayList<String>();
 
@@ -15,11 +15,20 @@ public abstract class Hero {
         this.levelAttributes = levelAttributes;
     }
     public abstract void levelUp();
-    public void equipWeapon(Weapon weapon){
-        equipment.put(Slot.Weapon ,weapon);
+    public void equipWeapon(Weapon weapon) {
+        if (this.level >= weapon.getRequiredLevel()) {
+            equipment.put(Slot.Weapon, weapon);
+        } else {
+            System.out.println("You can't equip that weapon yet! \n You need to be atleast lvl " + weapon.getRequiredLevel());
+        }
     }
     public void equipArmor(Slot slot, Armor armor){
-        equipment.put(slot, armor);
+        if(this.level >= armor.getRequiredLevel()){
+            equipment.put(slot, armor);
+        }
+        else{
+            System.out.println("You can't equip that armor yet! \n You need to be atleast lvl" + armor.getRequiredLevel());
+        }
     }
     public int calculateDamage (Hero hero){
         return this.level * 5;
@@ -32,6 +41,14 @@ public abstract class Hero {
     }
     @Override
     public String toString() {
-        return "[Name]: " + this.name;
+        return "[Name]: " + this.name + " [Lvl]:" + this.level;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
     }
 }
