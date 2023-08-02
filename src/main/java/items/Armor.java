9cdb1f65d0f32e23fee330ce8f117e4c0e352ad6
victorlun <victor.lundqvist@se.experis.com>
2023-08-02 15:15:38 +0200

    feat: armor class implemented

--- src/main/java/items/Armor.java
+++ src/main/java/items/Armor.java
@@ -1,5 +1,18 @@
 package items;
 
-public class Armor {
+import heroes.HeroAttribute;
 
+public class Armor extends Item {
+    private ArmorType armorType;
+    private HeroAttribute armorAttribute;
+    public Armor(String name, int requiredLevel, Slot slot, ArmorType armorType, HeroAttribute armorAttribute) {
+        super(name, requiredLevel, slot);
+        this.armorType = armorType;
+        this.armorAttribute = armorAttribute;
+    }
+
+    @Override
+    public String toString() {
+        return "{ARMOR} [Name]:" + getName() + " [Req.Lvl]:" + getRequiredLevel() + " [Slot]:" + getSlot() + " [Type]:" + armorType + " [Attribute]:" + armorAttribute;
+    }
 }
