62e617eb1ee5152971031f6e371bdd5e4b6deb5d
victorlun <victor.lundqvist@se.experis.com>
2023-08-02 15:15:57 +0200

    fix: added functionality to Item-class

--- src/main/java/items/Item.java
+++ src/main/java/items/Item.java
@@ -3,12 +3,33 @@ package items;
 public abstract class Item {
     private String name;
     private int requiredLevel;
-    private String slot;
+    private Slot slot;
 
-    public Item (String name, int requiredLevel, String slot){
+    public Item (String name, int requiredLevel, Slot slot){
         this.name = name;
         this.requiredLevel = requiredLevel;
         this.slot = slot;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                ", requiredLevel=" + requiredLevel +
+                ", slot='" + slot + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRequiredLevel() {
+        return requiredLevel;
+    }
+
+    public Slot getSlot() {
+        return slot;
     }
 }
 
