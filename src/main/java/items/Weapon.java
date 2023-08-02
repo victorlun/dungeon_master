882f5a9aa13615c025460f0b8b0d6dec1c916d20
victorlun <victor.lundqvist@se.experis.com>
2023-08-02 15:16:14 +0200

    feat: added functionality to Weapon-class

--- src/main/java/items/Weapon.java
+++ src/main/java/items/Weapon.java
@@ -1,10 +1,19 @@
 package items;
 
 public class Weapon extends Item{
-    private String type;
 
-    public Weapon(String name,int requiredLevel, String slot, String type){
+    private WeaponType type;
+    private int damage;
+
+
+    public Weapon(String name,int requiredLevel, Slot slot, WeaponType type, int damage){
         super(name, requiredLevel, slot);
         this.type = type;
+        this.damage = damage;
+    }
+
+    @Override
+    public String toString() {
+        return "{WEAPON} [Name]:" +  getName() + " [Req.Lvl]:" + getRequiredLevel() + " [Slot]:" + getSlot() + " [Type]:" + type + " [Damage]:"+ damage;
     }
 }
