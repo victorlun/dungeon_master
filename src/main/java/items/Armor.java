87e294715325d7e992141dc3f41163c29e4e07f4
victorlun <victor.lundqvist@se.experis.com>
2023-08-04 15:32:32 +0200

    fix: Armor & Item code cleanup and formatting

--- src/main/java/items/Armor.java
+++ src/main/java/items/Armor.java
@@ -1,24 +1,20 @@
 package items;
-
 import heroes.HeroAttribute;
 
 public class Armor extends Item {
-    private ArmorType armorType;
-    private HeroAttribute armorAttribute;
+    private final ArmorType armorType;
+    private final HeroAttribute armorAttribute;
     public Armor(String name, int requiredLevel, Slot slot, ArmorType armorType, HeroAttribute armorAttribute) {
         super(name, requiredLevel, slot);
         this.armorType = armorType;
         this.armorAttribute = armorAttribute;
     }
-
     public HeroAttribute getArmorAttribute() {
         return armorAttribute;
     }
-
     public ArmorType getArmorType() {
         return armorType;
     }
-
     @Override
     public String toString() {
         return "\n  [Name]:" + getName() + " [Req.Lvl]:" + getRequiredLevel() + " [Slot]:" + getSlot() + " [Type]:" + armorType + " [Attribute]:" + armorAttribute;
--- src/main/java/items/Item.java
+++ src/main/java/items/Item.java
@@ -1,16 +1,15 @@
 package items;
 
 public abstract class Item {
-    private String name;
-    private int requiredLevel;
-    private Slot slot;
+    private final String name;
+    private final int requiredLevel;
+    private final Slot slot;
 
     public Item (String name, int requiredLevel, Slot slot){
         this.name = name;
         this.requiredLevel = requiredLevel;
         this.slot = slot;
     }
-
     @Override
     public String toString() {
         return "Item{" +
@@ -19,15 +18,12 @@ public abstract class Item {
                 ", slot='" + slot + '\'' +
                 '}';
     }
-
     public String getName() {
         return name;
     }
-
     public int getRequiredLevel() {
         return requiredLevel;
     }
-
     public Slot getSlot() {
         return slot;
     }
