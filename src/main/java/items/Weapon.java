1edef3ddbe50eda591dc7b70acaf5ca63827b0bf
victorlun <victor.lundqvist@se.experis.com>
2023-08-04 15:33:23 +0200

    fix: Weapon code cleanup and formatting

--- src/main/java/items/Weapon.java
+++ src/main/java/items/Weapon.java
@@ -2,24 +2,20 @@ package items;
 
 public class Weapon extends Item{
 
-    private WeaponType type;
-    private int damage;
-
+    private final WeaponType type;
+    private final int damage;
 
     public Weapon(String name,int requiredLevel, Slot slot, WeaponType type, int damage){
         super(name, requiredLevel, slot);
         this.type = type;
         this.damage = damage;
     }
-
     public int getDamage() {
         return damage;
     }
-
     public WeaponType getType() {
         return type;
     }
-
     @Override
     public String toString() {
         return "Name:" +  getName() + " Lvl:" + getRequiredLevel() + " Slot:" + getSlot() + " Type:" + type + " Damage:"+ damage;
