d7ebd3a7abc580a81f6c48b049b4293bf04d7ba9
victorlun <victor.lundqvist@se.experis.com>
2023-08-04 15:05:47 +0200

    fix: weapon.toString() changed

--- src/main/java/items/Weapon.java
+++ src/main/java/items/Weapon.java
@@ -22,6 +22,6 @@ public class Weapon extends Item{
 
     @Override
     public String toString() {
-        return "\n  [Name]:" +  getName() + " [Req.Lvl]:" + getRequiredLevel() + " [Slot]:" + getSlot() + " [Type]:" + type + " [Damage]:"+ damage;
+        return "Name:" +  getName() + " Lvl:" + getRequiredLevel() + " Slot:" + getSlot() + " Type:" + type + " Damage:"+ damage;
     }
 }
